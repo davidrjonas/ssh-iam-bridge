a1f366d8b11af8fd36d812a9e830b75bb1fbe431
David Jonas <djonas@noip.com>
2016-11-16 02:23:36 -0800

    Remove get_instances()

--- main.go
+++ main.go
@@ -7,32 +7,8 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/aws/aws-sdk-go/service/iam"
 )
-
-func get_instances() {
-	sess, err := session.NewSession()
-	if err != nil {
-		panic(err)
-	}
-
-	svc := ec2.New(sess, &aws.Config{Region: aws.String("us-east-1")})
-
-	resp, err := svc.DescribeInstances(nil)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("> Number of reservation sets: ", len(resp.Reservations))
-
-	for idx, res := range resp.Reservations {
-		fmt.Println("  > Number of instances: ", len(res.Instances))
-		for _, inst := range resp.Reservations[idx].Instances {
-			fmt.Println("    - Instance ID: ", *inst.InstanceId)
-		}
-	}
-}
 
 func get_groups() {
 	sess, err := session.NewSession()
