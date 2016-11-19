94278aa380789af9bce841c184b03d8960ebb62a
David Jonas <djonas@noip.com>
2016-11-18 17:00:59 -0800

    Put arg parsing in place, define prefix

--- main.go
+++ main.go
@@ -7,9 +7,15 @@ import (
 	"fmt"
 	"os"
 
+	kingpin "gopkg.in/alecthomas/kingpin.v2"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/davidrjonas/ssh-iam-bridge/unix"
+	"github.com/kardianos/osext"
 )
+
+const VERSION = "1.0.0"
+const PREFIX = "system-"
 
 func awsToUnixId(aws_id *string) int {
 	// Treat the last 2 bytes of a sha256 hash of aws_id as an uint and add it to 2000
@@ -24,7 +30,7 @@ func awsToUnixId(aws_id *string) int {
 	return 2000 + (int(data) / 2)
 }
 
-func SyncGroups(prefix string) error {
+func syncGroups(prefix string) error {
 
 	role, err := GetIamRole()
 
@@ -62,6 +68,16 @@ func SyncGroups(prefix string) error {
 	return nil
 }
 
+func CreateUser(username string) error {
+	user, err := GetUser(username)
+
+	if err != nil {
+		return err
+	}
+
+	return unix.EnsureUser(username, awsToUnixId(user.UserId), aws.StringValue(user.Arn))
+}
+
 func GetAuthorizedKeys(username string) (*bytes.Buffer, error) {
 
 	keys, err := GetActiveSshPublicKeys(username)
@@ -87,20 +103,56 @@ func GetAuthorizedKeys(username string) (*bytes.Buffer, error) {
 	return &out, nil
 }
 
-func CreateUser(username string) error {
-	user, err := GetUser(username)
-
+func printAuthorizedKeys(username string) error {
+	buf, err := GetAuthorizedKeys(username)
 	if err != nil {
 		return err
 	}
-
-	return unix.EnsureUser(username, awsToUnixId(user.UserId), aws.StringValue(user.Arn))
+	buf.WriteTo(os.Stdout)
+	return nil
 }
 
-func main() {
-	buf, err := GetAuthorizedKeys("djonas")
+func pamCreateUser() error {
+	return nil
+}
+
+func getSelfPath() string {
+	self, err := osext.Executable()
 	if err != nil {
 		panic(err)
 	}
-	buf.WriteTo(os.Stdout)
+	return self
+}
+
+var (
+	installCommand       = kingpin.Command("install", "Install this program to authenticate SSH connections and create users")
+	installCommandUser   = installCommand.Arg("user", "The user under which to run the AuthorizedKeysCommand, will be created if it doesn't exit").String()
+	authKeysCommand      = kingpin.Command("authorized_keys", "Get the authorized_keys from IAM for user")
+	authKeysCommandUser  = authKeysCommand.Arg("user", "The IAM username for which to get keys").Required().String()
+	syncGroupsCommand    = kingpin.Command("sync_groups", "Sync the IAM groups with the local system groups")
+	pamCreateUserCommand = kingpin.Command("pam_create_user", "Create a user from the env during the sshd pam phase")
+	testCommand          = kingpin.Command("test", "")
+)
+
+func runTest() error {
+	fmt.Println(getSelfPath())
+	return nil
+}
+
+func main() {
+
+	kingpin.Version(VERSION)
+
+	switch kingpin.Parse() {
+	case installCommand.FullCommand():
+		install(getSelfPath(), *installCommandUser)
+	case authKeysCommand.FullCommand():
+		printAuthorizedKeys(*authKeysCommandUser)
+	case syncGroupsCommand.FullCommand():
+		syncGroups(PREFIX)
+	case pamCreateUserCommand.FullCommand():
+		pamCreateUser()
+	case testCommand.FullCommand():
+		runTest()
+	}
 }
